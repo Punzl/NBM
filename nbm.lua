19bea4600b0bc4dfbc9a6a4b8ddc651c492f31ed
punzl <mathias.szukitsch@gmail.com>
2025-09-17 15:23:44 +0200

    Spielerei

--- nbm.lua
+++ nbm.lua
@@ -1,3 +1,9 @@
+-- Das Offizielle <Naga Bums Mich> Gilden Addon! Beachtlich!
+
+local PREFIX = "NBM"
+
+local message_target_name = "Hexherr"
+
 -- Lade-Check
 local function Print(msg)
   DEFAULT_CHAT_FRAME:AddMessage("|cff00ff96HelloWorld:|r "..tostring(msg))
@@ -6,9 +12,13 @@ end
 -- Fensterbau
 local frame
 local function BuildFrame()
+    
+    -- Early Exit
     if frame then return end
+    
+    -- Frame:
     frame = CreateFrame("Frame", "HelloWorldFrame", UIParent)
-    frame:SetSize(200, 100)
+    frame:SetSize(400, 300)
     frame:SetPoint("CENTER")
     frame:SetBackdrop({
     bgFile = "Interface\\DialogFrame\\UI-DialogBox-Background",
@@ -18,19 +28,92 @@ local function BuildFrame()
     })
 
 
+    -- Text Header
     local line1 = frame:CreateFontString(nil, "OVERLAY", "GameFontNormalLarge")
-    line1:SetPoint("TOPLEFT", 16, -16)
-    line1:SetText("Test_String_1")
+    line1:SetPoint("TOP",frame, 0, -20)
+    line1:SetText("Naga Bums Mich")
 
+    -- Text paragraph
     local line2 = frame:CreateFontString(nil, "OVERLAY", "GameFontNormal")
-    line2:SetPoint("TOPLEFT", line1, "BOTTOMLEFT", 0, -4)
-    line2:SetText("Test_String_2")
+    line2:SetPoint("TOP", line1, 0, -20)
+    line2:SetText("Das Gilden-Addon")
 
+    -- Image
+    local img = frame:CreateTexture(nil, "BACKGROUND")
+    img:SetAllPoints()
+    img:SetTexture("Interface\\Icons\\INV_Misc_QuestionMark")
+    img:SetTexCoord(0.1, 0.9, 0.1, 0.9) 
+
+    -- Dropdown erstellen
+    local dd = CreateFrame("Frame", "MyAddonDropDown", frame, "UIDropDownMenuTemplate")
+    dd:SetPoint("CENTER", line2, 0, -40)
+
+    -- Data
+    local items = {
+        {text = "Willywerkel", value = "Willywerkel"},
+        {text = "Hexherr", value = "Hexherr"},
+        {text = "Herrow", value ="Herrow"},
+    }
+
+    -- Auswahl-Handler
+    local function OnClick(self)
+        UIDropDownMenu_SetSelectedValue(dd, self.value)
+        print("Gewählt:", self.value)
+        end
+
+    -- Initializer
+    local function Init(self, level)
+    local info
+    for _, it in ipairs(items) do
+        info = UIDropDownMenu_CreateInfo()
+        info.text = it.text
+        info.value = it.value
+        info.func = OnClick
+        info.checked = (UIDropDownMenu_GetSelectedValue(dd) == it.value)
+        UIDropDownMenu_AddButton(info, level)
+    end
+    end
+
+    UIDropDownMenu_Initialize(dd, Init)
+    UIDropDownMenu_SetWidth(dd, 140)
+    UIDropDownMenu_SetButtonWidth(dd, 160)
+    UIDropDownMenu_SetSelectedValue(dd, items[1].value)
+    UIDropDownMenu_JustifyText(dd, "LEFT")
+
+    -- Checkbox
     local cb = CreateFrame("CheckButton", nil, frame, "UICheckButtonTemplate")
-    cb:SetPoint("BOTTOMLEFT", 12, 12)
+    cb:SetPoint("BOTTOMLEFT", frame, 20, 20)
     local lbl = frame:CreateFontString(nil, "OVERLAY", "GameFontNormal")
     lbl:SetPoint("LEFT", cb, "RIGHT", 6, 0)
-    lbl:SetText("Aktiv")
+    lbl:SetText("Ist Taminos Mutter eine Hure?")
+    cb:SetScript("OnClick", function(self)
+        if self:GetChecked() then
+            print("Ja Sie ist eine Hure")
+        else
+            print("Jetzt ist Sie keine Hure")
+        end
+    end)
+
+
+    -- Button
+    local btn1 = CreateFrame("Button", "Btn1", frame, "UIPanelButtonTemplate")
+    btn1:SetSize(120,30)
+    btn1:SetPoint("Center", cb, 280, 0)
+    btn1:SetText("Click Me")
+    btn1:SetScript("OnClick", function(self,button,down)
+        SendAddonMessage(PREFIX, "DEBUG:Hello", "WHISPER", UIDropDownMenu_GetSelectedValue(dd))
+        print(UIDropDownMenu_GetSelectedValue(dd).." Button clicked")
+        end)
+
+    -- Empfang bei beiden Spielern
+    local f = CreateFrame("Frame")
+    f:RegisterEvent("CHAT_MSG_ADDON")
+    f:SetScript("OnEvent", function(_, _, prefix, msg, channel, sender)
+        if prefix ~= PREFIX then return end
+        if msg == "DEBUG:Hello" then
+            print("Empfangen von "..sender..": "..msg)
+            end
+        end)
 
 
     frame:Hide() -- erst per Slash zeigen
